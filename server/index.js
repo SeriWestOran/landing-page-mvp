00f820df0611cd2ab4190c92a33d35b20ac998a5
SeriWestOran <leghribrahim@gmail.com>
2026-09-10 23:38:24 +0100

    Set trust proxy for Express app

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const productRoutes = require("./routes/products");
 const authRoutes = require("./routes/auth");
 
 const app = express();
+app.set("trust proxy", 1);
 const PORT = process.env.PORT || 3000;
 
 // Parsers pour intercepter le JSON du login
