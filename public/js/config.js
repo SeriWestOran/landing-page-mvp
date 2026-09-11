a83e0c41303353d3de64a7804a78b7e323acd0b7
SeriWestOran <leghribrahim@gmail.com>
2026-09-11 11:06:02 +0100

    Fix heroSubtitle formatting in config.js

--- public/js/config.js
+++ public/js/config.js
@@ -2,7 +2,7 @@ window.SITE_CONFIG = {
   storeName: "BarakaShop محل البركة ",
   heroTitle: "سكان عدل 2500/2700 مرحبا بكم Des produits choisis, prêts à commander Ilot 9 Traka ou a la livraison",
   heroSubtitle:
-    "اكتشف تشكيلة الموسم. جميع المنتجات متاحة للشراء المباشر من المتجر أو عبر خدمة التوصيل عدل 2700/2500 مسكن Découvrez la sélection du moment. Chaque produit est disponible directement au magasin ou en livraison  AADL 2500/2700.",
+    "اكتشف تشكيلة الموسم. جميع المنتجات متاحة للشراء المباشر من المتجر أو عبر خدمة التوصيل عدل 2700/2500 مسكن Découvrez la sélection du moment. Chaque produit est disponible directement au magasin ou en livraison AADL 2500/2700.",
   heroImage: "/images/placeholder-1.svg",
   facebookPageUrl: "https://www.facebook.com/profile.php?id=100083308534021",
   contactText: "Une question ? Écrivez-nous sur Facebook ou sur whatsapp: 0779991160.",
