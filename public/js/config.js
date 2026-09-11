60f5578a771ba91596087343f048f39c95f6db65
SeriWestOran <leghribrahim@gmail.com>
2026-09-11 10:58:22 +0100

    Update heroImage to use placeholder image

--- public/js/config.js
+++ public/js/config.js
@@ -3,12 +3,8 @@ window.SITE_CONFIG = {
   heroTitle: "سكان عدل 2500/2700 مرحبا بكم Des produits choisis, prêts à commander Ilot 9 Traka ou a la livraison",
   heroSubtitle:
     "اكتشف تشكيلة الموسم. جميع المنتجات متاحة للشراء المباشر من المتجر أو عبر خدمة التوصيل عدل 2700/2500 مسكن Découvrez la sélection du moment. Chaque produit est disponible directement au magasin ou en livraison  AADL 2500/2700.",
-  heroImage: "/images/uploads/9c638789-f451-4a49-829e-7d3d0011222c.jpg",
+  heroImage: "/images/placeholder-1.svg",
   facebookPageUrl: "https://www.facebook.com/profile.php?id=100083308534021",
   contactText: "Une question ? Écrivez-nous sur Facebook ou sur whatsapp: 0779991160.",
-
-  // Numéro WhatsApp au format international, SANS le "+", sans espace,
-  // et sans le 0 initial du numéro local.
-  // Exemple Algérie : un numéro local 05 55 12 34 56 devient 213555123456
   whatsappNumber: "213779991160",
 };
